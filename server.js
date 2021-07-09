9a2ba587519c636098065491243951335bd32925
Debanjit Sengupta <rikrocks4@gmail.com>
2021-07-09 11:45:40 +0530

    Setup MongoDB

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
-const express = require("express");
+import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+
+dotenv.config();
+connectDB();
 
 const app = express();
+
+const PORT = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
 	res.send("Hello World");
 });
 
-app.listen(5000, () => {
-	console.log("Server running on port 5000");
+app.listen(PORT, () => {
+	console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
 });
