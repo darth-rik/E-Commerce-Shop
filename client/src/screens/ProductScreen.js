a9d8c0141940b54f6b42eb403c65f229b56b12ff
Debanjit Sengupta <rikrocks4@gmail.com>
2021-07-09 10:48:29 +0530

    Setup the backend

--- client/src/screens/ProductScreen.js
+++ client/src/screens/ProductScreen.js
@@ -1,7 +1,68 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { Row, Col, Image, ListGroup, Card, Button } from "react-bootstrap";
+import Rating from "../components/Rating";
+import products from "../products_and_images/products";
 
-const ProductScreen = () => {
-	return <div></div>;
+const ProductScreen = ({ match }) => {
+	const product = products.find((product) => product._id === match.params.id);
+
+	return (
+		<>
+			<Link className='btn btn-dark my-3' to='/'>
+				Go Back
+			</Link>
+			<Row>
+				<Col md={6}>
+					<Image src={product.image} fluid />
+				</Col>
+				<Col md={3}>
+					<ListGroup variant='flush'>
+						<ListGroup.Item>
+							<h3>{product.name}</h3>
+						</ListGroup.Item>
+						<ListGroup.Item>
+							<Rating
+								value={product.rating}
+								text={`${product.numReviews} reviews`}
+							/>
+						</ListGroup.Item>
+						<ListGroup.Item>Price: ${product.price}</ListGroup.Item>
+						<ListGroup.Item>Description: {product.description}</ListGroup.Item>
+					</ListGroup>
+				</Col>
+				<Col md={3}>
+					<Card>
+						<ListGroup variant='flush'>
+							<ListGroup.Item>
+								<Row>
+									<Col>Price:</Col>
+									<Col>
+										<strong>${product.price}</strong>
+									</Col>
+								</Row>
+							</ListGroup.Item>
+						</ListGroup>
+						<ListGroup variant='flush'>
+							<ListGroup.Item>
+								<Row>
+									<Col>Status:</Col>
+									<Col>
+										{product.countInStock > 0 ? "In Stock" : "Out of Stock"}
+									</Col>
+								</Row>
+							</ListGroup.Item>
+							<ListGroup.Item className='d-grid gap-2'>
+								<Button type='button' disabled={product.countInStock === 0}>
+									Add To Cart
+								</Button>
+							</ListGroup.Item>
+						</ListGroup>
+					</Card>
+				</Col>
+			</Row>
+		</>
+	);
 };
 
 export default ProductScreen;
