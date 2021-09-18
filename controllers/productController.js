59b4832f25c7cdb112e885a94cbd78e8fd8eb08f
Debanjit Sengupta <rikrocks4@gmail.com>
2021-09-18 19:04:02 +0530

    Implement pagination on backend

--- controllers/productController.js
+++ controllers/productController.js
@@ -12,17 +12,35 @@ export const getProductsByCategory = async (req, res) => {
         }
       : {};
 
+    const page = Number(req.query.page) || 1;
+    const limit = Number(req.query.limit) || 10;
+    const skip = (page - 1) * limit;
+
+    let count;
+
     let products;
 
     if (category === "all") {
-      products = await Product.find({ ...search });
+      count = await Product.countDocuments({ ...search });
+      products = await Product.find({ ...search })
+        .skip(skip)
+        .limit(limit);
     } else {
-      products = await Product.find({ category, ...search });
+      count = await Product.countDocuments({ category, ...search });
+      products = await Product.find({ category, ...search })
+        .skip(skip)
+        .limit(limit);
     }
 
-    res.json(products);
+    if (skip >= count) {
+      throw new Error("No more products to show");
+    }
+
+    const pages = Math.ceil(count / limit);
+
+    res.json({ products, pages });
   } catch (error) {
-    res.status(500).json({ message: "Server Error" });
+    res.status(404).json({ message: error.message });
   }
 };
 
