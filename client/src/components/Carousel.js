eab770457e9b30bc0346e916a73dd7e23bf61619
Debanjit Sengupta <rikrocks4@gmail.com>
2021-09-20 13:29:16 +0530

    Add navigation to carousel

--- client/src/components/Carousel.js
+++ client/src/components/Carousel.js
@@ -4,6 +4,8 @@ import Men from "../images/mens_ad.jpg";
 import Women from "../images/women's_ad.jpg";
 import Elec from "../images/electronics_ad.jpg";
 
+import { Link } from "react-router-dom";
+
 const ProductCarousel = () => {
   return (
     <Carousel variant="dark" indicators={false} className="mb-5">
@@ -17,7 +19,10 @@ const ProductCarousel = () => {
               New Arrivals
             </ListGroup.Item>{" "}
             <ListGroup.Item as="span">
-              <Button size="lg">Shop Now</Button>
+              <Link to="/products/category/men's%20clothing/page/1 ">
+                {" "}
+                <Button size="lg">Shop Now</Button>
+              </Link>
             </ListGroup.Item>
           </ListGroup>
         </Carousel.Caption>
@@ -32,7 +37,10 @@ const ProductCarousel = () => {
               New Season
             </ListGroup.Item>{" "}
             <ListGroup.Item as="span">
-              <Button size="lg">Shop Now</Button>
+              <Link to="/products/category/women's%20clothing/page/1">
+                {" "}
+                <Button size="lg">Shop Now</Button>
+              </Link>
             </ListGroup.Item>
           </ListGroup>
         </Carousel.Caption>
@@ -47,7 +55,9 @@ const ProductCarousel = () => {
               Exciting Offers
             </ListGroup.Item>{" "}
             <ListGroup.Item as="span">
-              <Button size="lg">Shop Now</Button>
+              <Link to="/products/category/electronics/page/1">
+                <Button size="lg">Shop Now</Button>
+              </Link>
             </ListGroup.Item>
           </ListGroup>
         </Carousel.Caption>
