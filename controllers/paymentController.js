2ad7cb63683c68cd3527edb1fc7355f43bda2665
Debanjit Sengupta <rikrocks4@gmail.com>
2021-09-22 01:58:32 +0530

    Modify checkout url

--- controllers/paymentController.js
+++ controllers/paymentController.js
@@ -25,8 +25,8 @@ export const getCheckoutSession = async (req, res) => {
 
     const session = await stripe.checkout.sessions.create({
       payment_method_types: ["card"],
-      success_url: `${req.protocol}://${req.hostname}:3000/order/${order._id}`,
-      cancel_url: `${req.protocol}://${req.hostname}:3000/order/${order._id}`,
+      success_url: `${req.protocol}://${req.hostname}/order/${order._id}`,
+      cancel_url: `${req.protocol}://${req.hostname}/order/${order._id}`,
 
       mode: "payment",
       customer_email: order.user.email,
